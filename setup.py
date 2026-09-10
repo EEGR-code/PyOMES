1b6f4e7a09d6c7370bac2fde2797ce50c8e4a25c
Ethan Errington <ethan.errington@kcl.ac.uk>
2026-09-10 10:56:48 +0100

    Remove VLsim backward-compatability shim VLsim was related to legacy naming of the package before release. References to it in tests, demos etc. have also been editted or removed accordingly.

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="PyOMES",
     version="0.12.5",
-    packages=find_packages(include=["PyOMES", "PyOMES.*", "VLsim", "VLsim.*"]),
+    packages=find_packages(include=["PyOMES", "PyOMES.*"]),
 )
